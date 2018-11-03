f5970bd45f2cdfc7b9a094c6b8bd35ac2abd2040
Luis Fernandes <sednanref2004@gmail.com>
2018-11-03 15:01:31 +0100

    New approach for 3.2

--- Ex2/code/Sheet2Exercise3.2.m
+++ Ex2/code/Sheet2Exercise3.2.m
@@ -8,9 +8,45 @@ function z = Sheet2Exercise3(n)
   % z = all nth roots of unity up to n
   
   assert(n >= 1,'n >= 1');
-  z = roots([1 zeros(1,n-1) -1]); % Find the roots for the x^n - 1 = 0 function
+
   
-  %% Plotting
-  plot(z)
+  % Getting all roots from 1 to n
+  for l = 1:n 
+    for j = 1:n  
+      if (j > l)
+        roots_n(l, j) = 0 + 0*i;
+      else
+        roots_n(l, j) = cos(2*pi*j/l) + i*sin(2*pi*j/l);
+      end
+    end
+  end
+  
+  
+  roots_n_n = roots_n(n,:);
+  primitives = [];
+  prim_counter = 1;
+  
+  % Checking if each member in roots of N is primitive, i.e. is not a root in 
+  % any previous roots 
+  for k = 1:n
+    possible_primitive = roots_n_n(k)
+    is_primitive = 1;
+    for j = 1:n-1
+      for l = 1:n
+        if (possible_primitive == roots_n_n(j,l))
+          is_primitive = 0;
+          break
+        end
+      end
+      if (is_primitive == 0)
+        break
+      end
+    end
+    if (is_primitive == 1) 
+      primitives(prim_counter) = possible_primitive;
+    end
+  end
+  
+  primitives
   
 end
